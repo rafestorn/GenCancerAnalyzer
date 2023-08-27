f9971996f8500f621819b1743cde36078d2da9d3
rafestorn <rafaestrada3@gmail.com>
2023-08-27 16:52:21 +0200

    FEAT: FUNCTION TO GET PROJECTS_IDS FROM GDC API

--- apps/home/apiGDC.py
+++ apps/home/apiGDC.py
@@ -1,10 +1,19 @@
 import requests
-import json
+
+status_endpt = 'https://api.gdc.cancer.gov/status'
+projects_endpt = 'https://api.gdc.cancer.gov/projects'
 
 def statusGDCApi():
-    status_endpt = 'https://api.gdc.cancer.gov/status'
     response = requests.get(status_endpt)
     if response.json()["status"] == "OK":
         return True
     else:
         return False
+    
+def getProjectsName():
+    params = {
+    "size": "100",
+    }
+    result = requests.get(projects_endpt, params = params).json()
+    ids = [{"id": e["project_id"], "name": e["name"]} for e in result["data"]["hits"]]
+    return ids
