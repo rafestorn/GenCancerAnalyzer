bb36ea032a16c2779ad1632285e940d664fcbb6f
rafestorn <rafaestrada3@gmail.com>
2023-08-28 16:16:36 +0200

    FEAT: GDC API FUNCTIONS

--- apps/home/apiGDC.py
+++ apps/home/apiGDC.py
@@ -10,10 +10,19 @@ def statusGDCApi():
     else:
         return False
     
-def getProjectsName():
+def getProjects():
     params = {
     "size": "100",
     }
     result = requests.get(projects_endpt, params = params).json()
+    return result
+    
+def getProjectsName():
+    result = getProjects()
     ids = [{"id": e["project_id"], "name": e["name"]} for e in result["data"]["hits"]]
     return ids
+
+def getProjectById(project_id):
+    response = requests.get(projects_endpt + "/" + project_id).json()['data']
+    result = {'primary_site': response}
+    return response
