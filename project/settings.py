739da7b0831286e1fb1598ec66a1f1ebedd02f30
rafestorn <rafaestrada3@gmail.com>
2023-09-15 12:25:36 +0200

    CONFIG: CELERY
    - Incorporate celery to the project
    - Hide env variables

--- project/settings.py
+++ project/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'drf_yasg',
     'apps.home',
+    'django_celery_results',
 ]
 
 MIDDLEWARE = [
@@ -148,3 +149,11 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configuración de Celery
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'UTC'
