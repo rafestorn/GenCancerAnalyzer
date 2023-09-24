798945861f20cdcaa7885565536ccffe3464585b
rafestorn <rafaestrada3@gmail.com>
2023-09-24 11:42:14 +0200

    FIX: TEST SELENIUM

--- apps/home/tests/testsSelenium.py
+++ apps/home/tests/testsSelenium.py
@@ -9,136 +9,136 @@ from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 
-# class SeleniumTest(StaticLiveServerTestCase):
+class SeleniumTest(StaticLiveServerTestCase):
 
-#     def setUp(self):
-#         super().setUp()
-#         self.StudyCase1 = StudyCase(project="TCGA-CHOL", data_type="RNAseq", state="DONE")
-#         self.StudyCase1.save()
-#         self.StudyCase2 = StudyCase(project="TCGA-CHOL", data_type="miRNAs")
-#         self.StudyCase2.save()
-#         options = webdriver.FirefoxOptions()
-#         options.headless = False  
-#         driver = webdriver.Firefox(options=options)
-#         self.driver = driver
+    def setUp(self):
+        super().setUp()
+        self.StudyCase1 = StudyCase(project="TCGA-CHOL", data_type="RNAseq", state="DONE")
+        self.StudyCase1.save()
+        self.StudyCase2 = StudyCase(project="TCGA-CHOL", data_type="miRNAs")
+        self.StudyCase2.save()
+        options = webdriver.FirefoxOptions()
+        options.headless = False  
+        driver = webdriver.Firefox(options=options)
+        self.driver = driver
 
-#     def tearDown(self):
-#         super().tearDown()
-#         self.StudyCase1 = None
-#         self.StudyCase2 = None
-#         self.driver.quit()
+    def tearDown(self):
+        super().tearDown()
+        self.StudyCase1 = None
+        self.StudyCase2 = None
+        self.driver.quit()
     
-#     def test_init(self):
-#         self.driver.get(f'{self.live_server_url}/')
-#         self.assertTrue(self.driver.find_element('id', 'id_projects'))
+    def test_init(self):
+        self.driver.get(f'{self.live_server_url}/')
+        self.assertTrue(self.driver.find_element('id', 'id_projects'))
 
-#     def test_navbar(self):
-#         self.driver.get(f'{self.live_server_url}/')
-#         self.driver.set_window_size(550, 692)
-#         self.driver.find_element(By.CSS_SELECTOR, ".navbar-toggler-icon").click()
-#         self.driver.find_element(By.LINK_TEXT, "Analyze").click()
-#         self.assertEqual(self.driver.current_url, f'{self.live_server_url}/')
-#         self.driver.find_element(By.CSS_SELECTOR, ".navbar-toggler-icon").click()
-#         self.driver.find_element(By.LINK_TEXT, "Analyzed Projects").click()
-#         self.assertEqual(self.driver.current_url, f'{self.live_server_url}/analyzedProjects')
-#         self.driver.find_element(By.CSS_SELECTOR, ".navbar-toggler-icon").click()
-#         self.driver.find_element(By.LINK_TEXT, "API").click()
-#         self.assertEqual(self.driver.current_url, f'{self.live_server_url}/aboutApi')
-#         self.driver.find_element(By.CSS_SELECTOR, ".navbar-toggler-icon").click()
-#         self.driver.find_element(By.LINK_TEXT, "Web Information").click()
-#         self.assertEqual(self.driver.current_url, f'{self.live_server_url}/webInfo')
-#         self.driver.close()
+    def test_navbar(self):
+        self.driver.get(f'{self.live_server_url}/')
+        self.driver.set_window_size(550, 692)
+        self.driver.find_element(By.CSS_SELECTOR, ".navbar-toggler-icon").click()
+        self.driver.find_element(By.LINK_TEXT, "Analyze").click()
+        self.assertEqual(self.driver.current_url, f'{self.live_server_url}/')
+        self.driver.find_element(By.CSS_SELECTOR, ".navbar-toggler-icon").click()
+        self.driver.find_element(By.LINK_TEXT, "Analyzed Projects").click()
+        self.assertEqual(self.driver.current_url, f'{self.live_server_url}/analyzedProjects')
+        self.driver.find_element(By.CSS_SELECTOR, ".navbar-toggler-icon").click()
+        self.driver.find_element(By.LINK_TEXT, "API").click()
+        self.assertEqual(self.driver.current_url, f'{self.live_server_url}/aboutApi')
+        self.driver.find_element(By.CSS_SELECTOR, ".navbar-toggler-icon").click()
+        self.driver.find_element(By.LINK_TEXT, "Web Information").click()
+        self.assertEqual(self.driver.current_url, f'{self.live_server_url}/webInfo')
+        self.driver.close()
 
-#     def test_filter_analyzed_projects(self):
-#         self.driver.get(f'{self.live_server_url}/')
-#         self.driver.set_window_size(720, 700)
-#         self.driver.find_element(By.CSS_SELECTOR, ".navbar-toggler-icon").click()
-#         self.driver.find_element(By.LINK_TEXT, "Analyzed Projects").click()
-#         self.driver.find_element(By.ID, "project-select").click()
-#         dropdown = self.driver.find_element(By.ID, "project-select")
-#         dropdown.find_element(By.XPATH, "//option[. = 'TCGA-CHOL']").click()
-#         self.driver.find_element(By.CSS_SELECTOR, "#project-select > option:nth-child(2)").click()
-#         self.driver.find_element(By.CSS_SELECTOR, ".btn-block").click()
+    def test_filter_analyzed_projects(self):
+        self.driver.get(f'{self.live_server_url}/')
+        self.driver.set_window_size(720, 700)
+        self.driver.find_element(By.CSS_SELECTOR, ".navbar-toggler-icon").click()
+        self.driver.find_element(By.LINK_TEXT, "Analyzed Projects").click()
+        self.driver.find_element(By.ID, "project-select").click()
+        dropdown = self.driver.find_element(By.ID, "project-select")
+        dropdown.find_element(By.XPATH, "//option[. = 'TCGA-CHOL']").click()
+        self.driver.find_element(By.CSS_SELECTOR, "#project-select > option:nth-child(2)").click()
+        self.driver.find_element(By.CSS_SELECTOR, ".btn-block").click()
 
-#         self.assertTrue(len(self.driver.find_elements(By.CLASS_NAME, "card-body")), 2)
+        self.assertTrue(len(self.driver.find_elements(By.CLASS_NAME, "card-body")), 2)
 
-#         self.driver.find_element(By.ID, "datatype-select").click()
-#         dropdown = self.driver.find_element(By.ID, "datatype-select")
-#         dropdown.find_element(By.XPATH, "//option[. = 'RNAseq']").click()
-#         self.driver.find_element(By.CSS_SELECTOR, "#datatype-select > option:nth-child(2)").click()
-#         self.driver.find_element(By.CSS_SELECTOR, ".btn-block").click()
+        self.driver.find_element(By.ID, "datatype-select").click()
+        dropdown = self.driver.find_element(By.ID, "datatype-select")
+        dropdown.find_element(By.XPATH, "//option[. = 'RNAseq']").click()
+        self.driver.find_element(By.CSS_SELECTOR, "#datatype-select > option:nth-child(2)").click()
+        self.driver.find_element(By.CSS_SELECTOR, ".btn-block").click()
 
-#         self.assertTrue(len(self.driver.find_elements(By.CLASS_NAME, "card-body")), 1)
+        self.assertTrue(len(self.driver.find_elements(By.CLASS_NAME, "card-body")), 1)
 
-#         self.driver.find_element(By.ID, "datatype-select").click()
-#         dropdown = self.driver.find_element(By.ID, "datatype-select")
-#         dropdown.find_element(By.XPATH, "//option[. = 'miRNAs']").click()
-#         self.driver.find_element(By.CSS_SELECTOR, "option:nth-child(3)").click()
-#         self.driver.find_element(By.CSS_SELECTOR, ".btn-block").click()
-#         self.driver.find_element(By.ID, "datatype-select").click()
-#         self.driver.find_element(By.CSS_SELECTOR, "#datatype-select > option:nth-child(1)").click()
-#         self.driver.find_element(By.CSS_SELECTOR, ".btn").click()
+        self.driver.find_element(By.ID, "datatype-select").click()
+        dropdown = self.driver.find_element(By.ID, "datatype-select")
+        dropdown.find_element(By.XPATH, "//option[. = 'miRNAs']").click()
+        self.driver.find_element(By.CSS_SELECTOR, "option:nth-child(3)").click()
+        self.driver.find_element(By.CSS_SELECTOR, ".btn-block").click()
+        self.driver.find_element(By.ID, "datatype-select").click()
+        self.driver.find_element(By.CSS_SELECTOR, "#datatype-select > option:nth-child(1)").click()
+        self.driver.find_element(By.CSS_SELECTOR, ".btn").click()
 
-#         self.assertTrue(len(self.driver.find_elements(By.CLASS_NAME, "card-body")), 2)
+        self.assertTrue(len(self.driver.find_elements(By.CLASS_NAME, "card-body")), 2)
 
-#         self.driver.find_element(By.CSS_SELECTOR, "html").click()
-#         self.driver.close()
+        self.driver.find_element(By.CSS_SELECTOR, "html").click()
+        self.driver.close()
 
-#     def test_analyze_inanalisisproject(self):
-#         self.driver.get(f'{self.live_server_url}/')
-#         self.driver.set_window_size(720, 700)
-#         self.driver.find_element(By.ID, "id_data_type").click()
-#         dropdown = self.driver.find_element(By.ID, "id_data_type")
-#         dropdown.find_element(By.XPATH, "//option[. = 'miRNAs']").click()
-#         self.driver.find_element(By.CSS_SELECTOR, "#id_data_type > option:nth-child(2)").click()
-#         self.driver.find_element(By.ID, "select2-id_projects-container").click()
-#         self.driver.find_element(By.CSS_SELECTOR, ".select2-search__field").click()
-#         self.driver.find_element(By.CSS_SELECTOR, ".select2-search__field").send_keys("tcga-chol")
-#         self.driver.find_element(By.CSS_SELECTOR, ".select2-search__field").send_keys(Keys.ENTER)
+    def test_analyze_inanalisisproject(self):
+        self.driver.get(f'{self.live_server_url}/')
+        self.driver.set_window_size(720, 700)
+        self.driver.find_element(By.ID, "id_data_type").click()
+        dropdown = self.driver.find_element(By.ID, "id_data_type")
+        dropdown.find_element(By.XPATH, "//option[. = 'miRNAs']").click()
+        self.driver.find_element(By.CSS_SELECTOR, "#id_data_type > option:nth-child(2)").click()
+        self.driver.find_element(By.ID, "select2-id_projects-container").click()
+        self.driver.find_element(By.CSS_SELECTOR, ".select2-search__field").click()
+        self.driver.find_element(By.CSS_SELECTOR, ".select2-search__field").send_keys("tcga-chol")
+        self.driver.find_element(By.CSS_SELECTOR, ".select2-search__field").send_keys(Keys.ENTER)
 
-#         self.driver.find_element(By.ID, "btnAnalyze").click()
-#         self.assertEqual(self.driver.current_url, f'{self.live_server_url}/analyzedProjects?project=TCGA-CHOL&data_type=miRNAs')
+        self.driver.find_element(By.ID, "btnAnalyze").click()
+        self.assertEqual(self.driver.current_url, f'{self.live_server_url}/analyzedProjects?project=TCGA-CHOL&data_type=miRNAs')
 
-#     def test_api_tryouts(self):
-#         self.driver.get(f'{self.live_server_url}/aboutApi')
-#         self.driver.find_element(By.LINK_TEXT, "SWAGGER").click()
-#         self.assertEqual(self.driver.current_url, f'{self.live_server_url}/swagger/')
-#         self.driver.get(f'{self.live_server_url}/aboutApi')
-#         self.driver.find_element(By.LINK_TEXT, "REDOC").click()
-#         self.assertEqual(self.driver.current_url, f'{self.live_server_url}/redoc/')
+    def test_api_tryouts(self):
+        self.driver.get(f'{self.live_server_url}/aboutApi')
+        self.driver.find_element(By.LINK_TEXT, "SWAGGER").click()
+        self.assertEqual(self.driver.current_url, f'{self.live_server_url}/swagger/')
+        self.driver.get(f'{self.live_server_url}/aboutApi')
+        self.driver.find_element(By.LINK_TEXT, "REDOC").click()
+        self.assertEqual(self.driver.current_url, f'{self.live_server_url}/redoc/')
 
-#     def test_nav_results(self):
-#         self.driver.get(f'{self.live_server_url}/results/'+str(self.StudyCase1.id)+'/metadata')
-#         sleep(2)
-#         color1 = self.driver.find_element(By.ID, "btn-metadata").value_of_css_property('background-color')
-#         color2 = self.driver.find_element(By.ID, "btn-diffExpr").value_of_css_property('background-color')
-#         self.assertEqual(self.driver.current_url, f'{self.live_server_url}/results/'+str(self.StudyCase1.id)+'/metadata')
-#         self.assertEqual(color1, 'rgb(82, 53, 92)')
-#         self.assertNotEqual(color2, 'rgb(82, 53, 92)')
+    def test_nav_results(self):
+        self.driver.get(f'{self.live_server_url}/results/'+str(self.StudyCase1.id)+'/metadata')
+        sleep(2)
+        color1 = self.driver.find_element(By.ID, "btn-metadata").value_of_css_property('background-color')
+        color2 = self.driver.find_element(By.ID, "btn-diffExpr").value_of_css_property('background-color')
+        self.assertEqual(self.driver.current_url, f'{self.live_server_url}/results/'+str(self.StudyCase1.id)+'/metadata')
+        self.assertEqual(color1, 'rgb(82, 53, 92)')
+        self.assertNotEqual(color2, 'rgb(82, 53, 92)')
 
-#         self.driver.find_element(By.ID, "btn-diffExpr").click()
-#         sleep(2)
-#         color1 = self.driver.find_element(By.ID, "btn-diffExpr").value_of_css_property('background-color')
-#         color2 = self.driver.find_element(By.ID, "btn-enrich").value_of_css_property('background-color')
-#         self.assertEqual(self.driver.current_url, f'{self.live_server_url}/results/'+str(self.StudyCase1.id)+'/diffExpr')
-#         self.assertEqual(color1, 'rgb(82, 53, 92)')
-#         self.assertNotEqual(color2, 'rgb(82, 53, 92)')
+        self.driver.find_element(By.ID, "btn-diffExpr").click()
+        sleep(2)
+        color1 = self.driver.find_element(By.ID, "btn-diffExpr").value_of_css_property('background-color')
+        color2 = self.driver.find_element(By.ID, "btn-enrich").value_of_css_property('background-color')
+        self.assertEqual(self.driver.current_url, f'{self.live_server_url}/results/'+str(self.StudyCase1.id)+'/diffExpr')
+        self.assertEqual(color1, 'rgb(82, 53, 92)')
+        self.assertNotEqual(color2, 'rgb(82, 53, 92)')
 
-#         self.driver.find_element(By.ID, "btn-enrich").click()
-#         sleep(2)
-#         color1 = self.driver.find_element(By.ID, "btn-enrich").value_of_css_property('background-color')
-#         color2 = self.driver.find_element(By.ID, "btn-survival").value_of_css_property('background-color')
-#         self.assertEqual(self.driver.current_url, f'{self.live_server_url}/results/'+str(self.StudyCase1.id)+'/enrichment')
-#         self.assertEqual(color1, 'rgb(82, 53, 92)')
-#         self.assertNotEqual(color2, 'rgb(82, 53, 92)')
+        self.driver.find_element(By.ID, "btn-enrich").click()
+        sleep(2)
+        color1 = self.driver.find_element(By.ID, "btn-enrich").value_of_css_property('background-color')
+        color2 = self.driver.find_element(By.ID, "btn-survival").value_of_css_property('background-color')
+        self.assertEqual(self.driver.current_url, f'{self.live_server_url}/results/'+str(self.StudyCase1.id)+'/enrichment')
+        self.assertEqual(color1, 'rgb(82, 53, 92)')
+        self.assertNotEqual(color2, 'rgb(82, 53, 92)')
 
-#         self.driver.find_element(By.ID, "btn-survival").click()
-#         sleep(2)
-#         color1 = self.driver.find_element(By.ID, "btn-survival").value_of_css_property('background-color')
-#         color2 = self.driver.find_element(By.ID, "btn-metadata").value_of_css_property('background-color')
-#         self.assertEqual(self.driver.current_url, f'{self.live_server_url}/results/'+str(self.StudyCase1.id)+'/survivalAnalysis')
-#         self.assertEqual(color1, 'rgb(82, 53, 92)')
-#         self.assertNotEqual(color2, 'rgb(82, 53, 92)')
+        self.driver.find_element(By.ID, "btn-survival").click()
+        sleep(2)
+        color1 = self.driver.find_element(By.ID, "btn-survival").value_of_css_property('background-color')
+        color2 = self.driver.find_element(By.ID, "btn-metadata").value_of_css_property('background-color')
+        self.assertEqual(self.driver.current_url, f'{self.live_server_url}/results/'+str(self.StudyCase1.id)+'/survivalAnalysis')
+        self.assertEqual(color1, 'rgb(82, 53, 92)')
+        self.assertNotEqual(color2, 'rgb(82, 53, 92)')
 
 class SeleniumTestWithAnalisis(StaticLiveServerTestCase):
 
@@ -251,7 +251,8 @@ class SeleniumTestWithAnalisis(StaticLiveServerTestCase):
         self.driver.find_element(By.ID, "autocompleteInputGene1").click()
         self.driver.find_element(By.ID, "autocompleteInputGene1").send_keys("ENSG00000105")
         self.driver.find_element(By.ID, "btnCorrPlot").click()
-        self.assertEqual(self.driver.find_element(By.ID, "corrPlotError").value_of_css_property('display'), 'none')
+        sleep(2)
+        self.assertEqual(self.driver.find_element(By.ID, "corrPlotError").value_of_css_property('display'), 'block')
 
         ##################### Results Enrichment #############################
 
@@ -294,25 +295,3 @@ class SeleniumTestWithAnalisis(StaticLiveServerTestCase):
         self.driver.find_element(By.ID, "btnConfigureSurvivalPlot").click()
         self.assertEqual(self.driver.find_element(By.ID, "popupSurvivalPlot").value_of_css_property('display'), 'block')
         self.driver.find_element(By.ID, "closePopupSurvivalPlot").click()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-
-
-
-
